d0603a6a9a2cdebb1e136090f4ca8e50d1f7519d
lsh1752 <leesunghyuck0@gmail.com>
2024-12-30 21:40:04 +0900

    feat: chapter03,04 error, chapter05 not error

--- leesihu_springboot/chapter04/src/test/java/me/leesihu/springbootdeveloper/QuizControllerTest.java
+++ leesihu_springboot/chapter04/src/test/java/me/leesihu/springbootdeveloper/QuizControllerTest.java
@@ -1,6 +1,7 @@
 package me.leesihu.springbootdeveloper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.apache.bcel.classfile.Code;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
+
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -46,10 +48,10 @@ class QuizControllerTest {
         final String url = "/quiz";
         // when
         final ResultActions result = mockMvc.perform(get(url)
-                .param("code","1")                  // valuees가 "1"인 이유는 JSON이라서
+                .param("code", "1")                 // values가 "1"인 이유는 JSON이라서
         );
         // then
-        result.andExpect(status().isBadRequest())
+        result.andExpect(status().isCreated())
                 .andExpect(content().string("Created!"));
     }
 
@@ -60,61 +62,65 @@ class QuizControllerTest {
         final String url = "/quiz";
 
         // when
-        final ResultActions result = mockMvc.perform(get(url)       // getQuiz1()에서 이미 get() import
-                .param("code","2")
+        final ResultActions result = mockMvc.perform(get(url)           // getQuiz1()에서 이미 get() import
+                .param("code", "2")
         );
 
         // then
-        result.andExpect(status().isAccepted())
+        result.andExpect(status().isBadRequest())
                 .andExpect(content().string("Bad Request!"));
     }
 
-    @DisplayName("quiz() : POST / quiz?code=1 이면 응답 코드는 403, 응답 본문은 Forbidden!을 반환한다.")
+    @DisplayName("quiz() : POST /quiz?code=1 이면 응답 코드는 403, 응답 본문은 Forbidden!을 반환한다.")
     @Test
     public void postQuiz1() throws Exception {
         // 여기다가 답 작성하면 됩니다.
         // given
         final String url = "/quiz";
-        // when     -> 이 부분에 대한 해석이 꼬ㅒ 중요합니다. -> 다음주 설명 예정
-        final ResultActions resultActions = mockMvc.perform(post(url)
+        // when     -> 이 부분에 대한 해석이 꽤 중요합니다. -> 다음주 설명 예정
+        final ResultActions result = mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(new Code(1)))
         );
         // then
         result.andExpect(status().isForbidden())
                 .andExpect(content().string("Forbidden!"));
+
     }
 
-    @DisplayName("quiz() : POST /quiz?code=2 이면 응답 코드는 200, 응답 본문은 OK!를 반환한다")
+    @DisplayName("quiz() : POST /quiz?code=99 이면 응답 코드는 200, 응답 본문은 OK!를 반환한다")
     @Test
     public void postQuiz2() throws Exception {
-        // 여기다가 답 작성하면 됩니다.
+        //여기다가 답 작성하시면 됩니다.
         // given
         final String url = "/quiz";
         // when
-        final ResultActions resultActions = mockMvc.perform(post(url)
+        final ResultActions result = mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(new Code(2)))
+                .content(objectMapper.writeValueAsString(new Code(99)))
         );
         // then
-        result.andExpect(status().isForbidden())
+        result.andExpect(status().isOk())
                 .andExpect(content().string("OK!"));
     }
+
+
+
 }
 /*
     ObjectMapper : Jackson 라이브러리에서 제공하는 클래스로 객체와 JSON간 변환을 처리
 
-    Code code = new Code (13)           // QuizController.java에서 확인
-    objectMapper.writeAsString(code)
+    Code code = new Code(13)            // QuizController.java에서 확인
+    objectMapper.writeValueAsString(code)
 
     이상의 예시 코드를 분석해보면,
     new Code(13)를 통해서 Code 클래스의 code 객체를 생성했고,
     writeValueAsString(code) 메서드를 통해서 JSON 문자열 형태로 객체가 변환.
 
     이런식으로,
-    객체 -> JSON 문자열로의 변환을 '객체 직렬화'라고 하기도 합니다.
+    객체 → JSON 문자열로의 변환을 '객체 직렬화'라고 하기도 합니다.
 
-    jSON 문자열로의 변환 결과 예시
+    JSON 문자열로의 변환 결과 예시
     {
         'value': 13
     }
